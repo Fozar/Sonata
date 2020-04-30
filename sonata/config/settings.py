96f36334f35dbd449432f841138b52a558a3c079
Fozar <fozar97@mail.ru>
2020-04-30 21:53:13 +0700

    Changed bot desc

--- sonata/config/settings.py
+++ sonata/config/settings.py
@@ -6,9 +6,31 @@ class BotConfig:
     test_token: str = None
     owner_id: int = 149722383165423616
     description: str = _(
-        """Sonata is a Discord multi-functional polyglot bot. The bot has many "cogs" \
-        for administering and moderating the guild, entertaining guild members, \
-        providing latest information from the worldwide network, and much more."""
+        """Sonata is a multi-functional polyglot bot. The bot has many modules for \
+        entertaining members, providing useful utilities, useful information from a \
+        worldwide network and much more.
+        
+        The sonata has a global leveling system. Users can gain experience for activity \
+        and increase levels on different servers to which the bot is connected. This \
+        red-haired girl keeps various statistics. Thanks to this, you can find out the \
+        average number of messages per day in the guild, how many teams were used, which \
+        emoji is the most popular, etc. A brief of each of the modules:
+        
+        `Admin` This module is responsible for the unique behavior of the bot on your \
+        server. You can disable and enable modules and commands, change the language and \
+        prefix of the bot, etc.
+        `Emoji` The module allows you to get detailed information about emoji, and also \
+        keeps statistics on the use of emoji.
+        `Reminder` This module allows you to create reminders for a specific event after \
+        a certain amount of time.
+        `Utils` Various useful utilities, such as a calculator, weather and virus info.
+        `Stats` The module is responsible for general statistics and level system. It \
+        has user rank card command and a leaderboard.
+        `Locale` This module allows you to set the language in direct messages and \
+        display a list of supported languages.
+        `Fun` A large set of entertainment teams. Everything from 8ball to cat pictures. \
+        Also has an advanced version of roll.
+        `General` Main commands and information."""
     )
     default_prefix: str = "!"
     core_cogs: FrozenSet[str] = frozenset(

bf995dccc494993554bbcfadd0a846914e77e8b2
Fozar <fozar97@mail.ru>
2020-05-05 22:30:24 +0700

    Owner and guild admins can use Mod cog

--- sonata/bot/core/checks.py
+++ sonata/bot/core/checks.py
@@ -26,15 +26,21 @@ def premium_only():
 
 def is_mod():
     async def predicate(ctx: Context):
-        guild = await ctx.db.guilds.find_one({"id": ctx.guild.id}, {"mod_roles": True})
+        guild = await ctx.db.guilds.find_one(
+            {"id": ctx.guild.id}, {"mod_roles": True, "admin_roles": True}
+        )
         if (
             guild
             and guild.get("admin_roles")
             and discord.utils.find(
-                lambda role: role.id in guild["mod_roles"], ctx.author.roles
+                lambda role: role.id
+                in set(guild["mod_roles"]) | set(guild["admin_roles"]),
+                ctx.author.roles,
             )
             is not None
         ):
+            return True
+        if await ctx.bot.is_owner(ctx.author):
             return True
         return False
 

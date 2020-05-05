5cf10fb440ee7ee415e552d92a183526dc049cb3
Fozar <fozar97@mail.ru>
2020-05-05 22:31:56 +0700

    Added ModlogCaseConverter. Converters sorted in alphabetical order

--- sonata/bot/utils/converters.py
+++ sonata/bot/utils/converters.py
@@ -15,6 +15,7 @@ from discord.ext import commands
 
 from sonata.bot import core
 from . import i18n
+from ...db.models import ModlogCase
 
 
 def flag_to_locale(flag: str) -> str:
@@ -41,6 +42,43 @@ def locale_to_lang(locale: str) -> str:
     return locale[:2]
 
 
+class EvalExpression(commands.Converter):
+    def __init__(self):
+        self.parsed = None
+        self.fn_name = "_eval_expr"
+
+    def insert_returns(self, body):
+        # insert return stmt if the last expression is a expression statement
+        if isinstance(body[-1], ast.Expr):
+            body[-1] = ast.Return(body[-1].value)
+            ast.fix_missing_locations(body[-1])
+
+        # for if statements, we insert returns into the body and the orelse
+        if isinstance(body[-1], ast.If):
+            self.insert_returns(body[-1].body)
+            self.insert_returns(body[-1].orelse)
+
+        # for with blocks, again we insert returns into the body
+        if isinstance(body[-1], ast.With):
+            self.insert_returns(body[-1].body)
+
+    async def convert(self, ctx: core.Context, argument):
+        cmd = argument.strip("` ").splitlines()
+        if cmd[0] in ("py", "python"):
+            cmd.pop(0)
+        # add a layer of indentation
+        cmd = "\n".join(f"    {i}" for i in cmd)
+
+        # wrap in async def body
+        body = f"async def {self.fn_name}():\n{cmd}"
+
+        self.parsed = ast.parse(body)
+        body = self.parsed.body[0].body
+
+        self.insert_returns(body)
+        return self
+
+
 class GlobalChannel(commands.Converter):
     async def convert(self, ctx, argument):
         try:
@@ -60,6 +98,128 @@ class GlobalChannel(commands.Converter):
                         _("Could not find a channel by ID {0}.").format(repr(argument))
                     )
                 return channel
+
+
+class MathExpression(commands.Converter):
+    def __init__(self, timeout: float = 1.0):
+        self.operators = {
+            ast.Add: op.add,
+            ast.Sub: op.sub,
+            ast.Mult: op.mul,
+            ast.Div: op.truediv,
+            ast.Pow: self.power,
+            ast.USub: op.neg,
+            ast.UAdd: op.pos,
+        }
+        self.timeout = timeout
+
+    @staticmethod
+    def power(a, b):
+        if any(abs(n) > 100 for n in [a, b]):
+            raise OverflowError(a, b)
+        return op.pow(a, b)
+
+    def eval_(self, node):
+        if isinstance(node, ast.Num):  # <number>
+            return Decimal(str(node.n))
+        elif isinstance(node, ast.BinOp):  # <left> <operator> <right>
+            return self.operators[type(node.op)](
+                self.eval_(node.left), self.eval_(node.right)
+            )
+        elif isinstance(node, ast.UnaryOp):  # <operator> <operand> e.g., -1
+            return self.operators[type(node.op)](self.eval_(node.operand))
+        elif isinstance(node, ast.Name):
+            return Decimal(node.id)
+        else:
+            raise commands.BadArgument(_("Invalid expression"))
+
+    def eval_expr(self, expr):
+        try:
+            with localcontext(ExtendedContext):
+                return self.eval_(ast.parse(expr, mode="eval").body)
+        except SyntaxError:
+            raise commands.BadArgument(_("Invalid expression"))
+
+    async def convert(self, ctx: core.Context, argument):
+        argument = argument.strip(" `\n").replace(" ", "").replace("^", "**")
+        eval_expr = partial(self.eval_expr, argument)
+        with ctx.typing():
+            with concurrent.futures.ThreadPoolExecutor() as pool:
+                try:
+                    result = await asyncio.wait_for(
+                        ctx.bot.loop.run_in_executor(pool, eval_expr),
+                        timeout=self.timeout,
+                        loop=ctx.bot.loop,
+                    )
+                except asyncio.TimeoutError:
+                    raise commands.BadArgument(
+                        _("Timeout exceeded: {0}s").format(self.timeout)
+                    )
+                except OverflowError as e:
+                    raise commands.BadArgument(
+                        _("Range exceeded: {0} ** {1}").format(e.args[0], e.args[1])
+                    )
+        return result
+
+
+class ModeratedMember(commands.MemberConverter):
+    async def convert(self, ctx: core.Context, argument):
+        member = await super().convert(ctx, argument)
+        if ctx.author == member:
+            raise commands.BadArgument(
+                _('Member "{0}" is the author of the message.').format(str(member))
+            )
+        if ctx.guild.me == member:
+            raise commands.BadArgument(_('Member "{0}" is me.').format(str(member)))
+        if ctx.guild.owner == member:
+            raise commands.BadArgument(
+                _('Member "{0}" is the guild owner.').format(str(member))
+            )
+        if ctx.guild.owner == ctx.author:
+            return member
+        if ctx.author.top_role <= member.top_role:
+            raise commands.BadArgument(
+                _('Member "{0}" is above moderator in the role hierarchy.').format(
+                    str(member)
+                )
+            )
+        if ctx.guild.me.top_role <= member.top_role:
+            raise commands.BadArgument(
+                _('Member "{0}" is above me in the role hierarchy.').format(str(member))
+            )
+        if ctx.author.guild_permissions.administrator:
+            return member
+        if member.guild_permissions.administrator:
+            raise commands.BadArgument(
+                _('Member "{0}" is guild administrator.').format(str(member))
+            )
+        guild = await ctx.db.guilds.find_one(
+            {"id": ctx.guild.id}, {"admin_roles": True, "mod_roles": True}
+        )
+        if not guild:
+            return member
+
+        if guild.get("admin_roles") and member.id in guild["admin_roles"]:
+            raise commands.BadArgument(
+                _('Member "{0}" is guild administrator.').format(str(member))
+            )
+        if guild.get("mod_roles") and member.id in guild["mod_roles"]:
+            raise commands.BadArgument(
+                _('Member "{0}" is guild moderator.').format(str(member))
+            )
+        return member
+
+
+class ModlogCaseConverter(commands.IDConverter):
+    async def convert(self, ctx: core.Context, argument):
+        match = self._get_id_match(argument)
+        if match is None:
+            raise commands.BadArgument(_("Invalid ID."))
+        case = await ctx.db.modlog_cases.find_one({"guild_id": ctx.guild.id, "id": int(argument)})
+        if not case:
+            raise commands.BadArgument(_("Case not found."))
+
+        return ModlogCase(**case)
 
 
 class UserFriendlyTime(commands.Converter):
@@ -127,150 +287,3 @@ class UserFriendlyTime(commands.Converter):
             self.arg = remaining
 
         return self
-
-
-class MathExpression(commands.Converter):
-    def __init__(self, timeout: float = 1.0):
-        self.operators = {
-            ast.Add: op.add,
-            ast.Sub: op.sub,
-            ast.Mult: op.mul,
-            ast.Div: op.truediv,
-            ast.Pow: self.power,
-            ast.USub: op.neg,
-            ast.UAdd: op.pos,
-        }
-        self.timeout = timeout
-
-    @staticmethod
-    def power(a, b):
-        if any(abs(n) > 100 for n in [a, b]):
-            raise OverflowError(a, b)
-        return op.pow(a, b)
-
-    def eval_(self, node):
-        if isinstance(node, ast.Num):  # <number>
-            return Decimal(str(node.n))
-        elif isinstance(node, ast.BinOp):  # <left> <operator> <right>
-            return self.operators[type(node.op)](
-                self.eval_(node.left), self.eval_(node.right)
-            )
-        elif isinstance(node, ast.UnaryOp):  # <operator> <operand> e.g., -1
-            return self.operators[type(node.op)](self.eval_(node.operand))
-        elif isinstance(node, ast.Name):
-            return Decimal(node.id)
-        else:
-            raise commands.BadArgument(_("Invalid expression"))
-
-    def eval_expr(self, expr):
-        try:
-            with localcontext(ExtendedContext):
-                return self.eval_(ast.parse(expr, mode="eval").body)
-        except SyntaxError:
-            raise commands.BadArgument(_("Invalid expression"))
-
-    async def convert(self, ctx: core.Context, argument):
-        argument = argument.strip(" `\n").replace(" ", "").replace("^", "**")
-        eval_expr = partial(self.eval_expr, argument)
-        with ctx.typing():
-            with concurrent.futures.ThreadPoolExecutor() as pool:
-                try:
-                    result = await asyncio.wait_for(
-                        ctx.bot.loop.run_in_executor(pool, eval_expr),
-                        timeout=self.timeout,
-                        loop=ctx.bot.loop,
-                    )
-                except asyncio.TimeoutError:
-                    raise commands.BadArgument(
-                        _("Timeout exceeded: {0}s").format(self.timeout)
-                    )
-                except OverflowError as e:
-                    raise commands.BadArgument(
-                        _("Range exceeded: {0} ** {1}").format(e.args[0], e.args[1])
-                    )
-        return result
-
-
-class EvalExpression(commands.Converter):
-    def __init__(self):
-        self.parsed = None
-        self.fn_name = "_eval_expr"
-
-    def insert_returns(self, body):
-        # insert return stmt if the last expression is a expression statement
-        if isinstance(body[-1], ast.Expr):
-            body[-1] = ast.Return(body[-1].value)
-            ast.fix_missing_locations(body[-1])
-
-        # for if statements, we insert returns into the body and the orelse
-        if isinstance(body[-1], ast.If):
-            self.insert_returns(body[-1].body)
-            self.insert_returns(body[-1].orelse)
-
-        # for with blocks, again we insert returns into the body
-        if isinstance(body[-1], ast.With):
-            self.insert_returns(body[-1].body)
-
-    async def convert(self, ctx: core.Context, argument):
-        cmd = argument.strip("` ").splitlines()
-        if cmd[0] in ("py", "python"):
-            cmd.pop(0)
-        # add a layer of indentation
-        cmd = "\n".join(f"    {i}" for i in cmd)
-
-        # wrap in async def body
-        body = f"async def {self.fn_name}():\n{cmd}"
-
-        self.parsed = ast.parse(body)
-        body = self.parsed.body[0].body
-
-        self.insert_returns(body)
-        return self
-
-
-class ModeratedMember(commands.MemberConverter):
-    async def convert(self, ctx: core.Context, argument):
-        member = await super().convert(ctx, argument)
-        if ctx.author == member:
-            raise commands.BadArgument(
-                _('Member "{0}" is the author of the message.').format(str(member))
-            )
-        if ctx.guild.me == member:
-            raise commands.BadArgument(_('Member "{0}" is me.').format(str(member)))
-        if ctx.guild.owner == member:
-            raise commands.BadArgument(
-                _('Member "{0}" is the guild owner.').format(str(member))
-            )
-        if ctx.guild.owner == ctx.author:
-            return member
-        if ctx.author.top_role <= member.top_role:
-            raise commands.BadArgument(
-                _('Member "{0}" is above moderator in the role hierarchy.').format(
-                    str(member)
-                )
-            )
-        if ctx.guild.me.top_role <= member.top_role:
-            raise commands.BadArgument(
-                _('Member "{0}" is above me in the role hierarchy.').format(str(member))
-            )
-        if ctx.author.guild_permissions.administrator:
-            return member
-        if member.guild_permissions.administrator:
-            raise commands.BadArgument(
-                _('Member "{0}" is guild administrator.').format(str(member))
-            )
-        guild = await ctx.db.guilds.find_one(
-            {"id": ctx.guild.id}, {"admin_roles": True, "mod_roles": True}
-        )
-        if not guild:
-            return member
-
-        if guild.get("admin_roles") and member.id in guild["admin_roles"]:
-            raise commands.BadArgument(
-                _('Member "{0}" is guild administrator.').format(str(member))
-            )
-        if guild.get("mod_roles") and member.id in guild["mod_roles"]:
-            raise commands.BadArgument(
-                _('Member "{0}" is guild moderator.').format(str(member))
-            )
-        return member

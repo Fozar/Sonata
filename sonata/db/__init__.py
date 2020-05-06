5a189613b02b482fc192d4a1b3817169eae4fbfb
Fozar <fozar97@mail.ru>
2020-05-06 18:02:11 +0700

    Added appname and loop to mongo client

--- sonata/db/__init__.py
+++ sonata/db/__init__.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from motor import motor_asyncio as motorio
 
 from sonata.db.logging import setup_logger
@@ -9,8 +11,8 @@ async def close_mongo(app):
 
 async def init_db(app):
     setup_logger()
-    app["db"] = motorio.AsyncIOMotorClient(app["config"]["mongo"].url)[
-        app["config"]["mongo"].database
-    ]
+    app["db"] = motorio.AsyncIOMotorClient(
+        app["config"]["mongo"].url, appname="sonata", io_loop=asyncio.get_event_loop()
+    )[app["config"]["mongo"].database]
     app["logger"].info("Mongo connected.")
     app.on_cleanup.append(close_mongo)

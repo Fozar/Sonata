212968dd39bb896bf6ced2e98f244c762fe10792
Fozar <fozar97@mail.ru>
2020-05-27 00:49:48 +0700

    Minor refactoring twitch.py

--- sonata/bot/cogs/streams/twitch.py
+++ sonata/bot/cogs/streams/twitch.py
@@ -7,11 +7,11 @@ from typing import Optional
 import discord
 import twitch
 from discord.ext import commands
-from pymongo import ReturnDocument
+from twitch.webhook import StreamChanged
 
 from sonata.bot import core
 from sonata.bot.core import errors
-from sonata.db.models import TwitchSubscription, SubscriptionAlertConfig
+from sonata.db.models import TwitchSubscriptionStatus, SubscriptionAlertConfig
 
 HELIX_API = "https://api.twitch.tv/helix"
 STREAMS_URL = HELIX_API + "/streams"
@@ -35,9 +35,9 @@ class TwitchUserConverter(commands.Converter):
     async def convert(self, ctx: core.Context, argument):
         client = ctx.bot.twitch_client
         try:
-            user = await client.get_user(int(argument))
+            user = await client.get_user(str(int(argument)))
         except ValueError:
-            user = await client.get_user(user_login=argument)
+            user = await client.get_user(login=argument)
         if user is None:
             raise commands.BadArgument(_("Twitch user is not found"))
         return user
@@ -70,11 +70,11 @@ class TwitchMixin(core.Cog):
     @core.Cog.listener()
     async def on_stream_changed(self, data: Optional[dict], user_id: str):
         topic = STREAMS_URL + f"?user_id={user_id}"
-        sub = await self.sonata.db.twitch_subs.find_one(
+        sub_status = await self.sonata.db.twitch_subs.find_one(
             {"topic": topic}, {"guilds": True}
         )
         try:
-            guilds = sub["guilds"]
+            guilds = sub_status["guilds"]
         except (KeyError, TypeError):
             return
 
@@ -87,19 +87,14 @@ class TwitchMixin(core.Cog):
                 continue
 
             if not data:
-                if not alert_conf["message_id"]:
-                    continue
-                try:
-                    await self.close_alert(guild, alert_conf, user_id)
-                except discord.HTTPException:
-                    pass
-                else:
-                    await self.sonata.db.twitch_subs.update_one(
-                        {"topic": topic, "guilds.id": guild.id},
-                        {"$set": {"guilds.$.message_id": None}},
-                    )
-                finally:
-                    continue
+                if alert_conf["message_id"]:
+                    with suppress(discord.HTTPException):
+                        await self.close_alert(guild, alert_conf, user_id)
+                        await self.sonata.db.twitch_subs.update_one(
+                            {"topic": topic, "guilds.id": guild.id},
+                            {"$set": {"guilds.$.message_id": None}},
+                        )
+                continue
 
             stream = twitch.Stream(self.twitch, data)
             try:
@@ -234,20 +229,25 @@ class TwitchMixin(core.Cog):
         return await channel.send(content=content, embed=embed)
 
     async def extend_subscription(
-        self, sub: TwitchSubscription, lease_seconds: int = 864000
+        self, sub_status: TwitchSubscriptionStatus, lease_seconds: int = 864000
     ):
         try:
-            await self.twitch.subscribe_to_events(
-                sub.callback, sub.topic, lease_seconds, sub.secret
+            topic = twitch.StreamChanged.from_uri(sub_status.topic)
+            subscription = self.twitch.create_subscription(
+                sub_status.callback,
+                topic,
+                lease_seconds,
+                self.sonata.config["twitch"].hub_secret,
             )
+            await subscription.extend()
             expires_at = datetime.utcnow() + timedelta(seconds=lease_seconds)
             await self.sonata.db.twitch_subs.update_one(
-                {"topic": sub.topic}, {"$set": {"expires_at": expires_at}},
+                {"topic": sub_status.topic}, {"$set": {"expires_at": expires_at}},
             )
             return True
         except twitch.HTTPException:
             await self.sonata.db.twitch_subs.update_one(
-                {"topic": sub.topic}, {"$set": {"verified": False}},
+                {"topic": sub_status.topic}, {"$set": {"verified": False}},
             )
             return False
 
@@ -260,16 +260,16 @@ class TwitchMixin(core.Cog):
             sort=[("expires_at", 1)],
         )
         if await cursor.fetch_next:
-            sub = cursor.next_object()
-            return TwitchSubscription(**sub)
+            sub_status = cursor.next_object()
+            return TwitchSubscriptionStatus(**sub_status)
         else:
             return None
 
     async def wait_for_active_subs(self, *, days=10):
-        sub = await self.get_active_sub(days=days)
-        if sub is not None:
+        sub_status = await self.get_active_sub(days=days)
+        if sub_status is not None:
             self._have_data.set()
-            return sub
+            return sub_status
 
         self._have_data.clear()
         self._next_sub = None
@@ -279,25 +279,28 @@ class TwitchMixin(core.Cog):
     async def dispatch_subs(self):
         try:
             while not self.sonata.is_closed():
-                sub = self._next_sub = await self.wait_for_active_subs(days=40)
+                sub_status = self._next_sub = await self.wait_for_active_subs(days=40)
                 now = datetime.utcnow()
-                if sub.expires_at >= now:
-                    to_sleep = (sub.expires_at - now).total_seconds()
+                if sub_status.expires_at >= now:
+                    to_sleep = (sub_status.expires_at - now).total_seconds()
                     await asyncio.sleep(to_sleep)
 
-                await self.extend_subscription(sub)
+                await asyncio.sleep(30)
+                await self.extend_subscription(sub_status)
         except asyncio.CancelledError:
             raise
         except (OSError, discord.ConnectionClosed):
             self._task.cancel()
             self._task = self.sonata.loop.create_task(self.dispatch_subs())
 
-    async def is_subscription_exist(self, topic: str):
-        cursor = self.sonata.db.twitch_subs.find({"topic": topic}, {"id": True})
+    async def is_subscription_exist(self, topic: twitch.webhook.Topic):
+        cursor = self.sonata.db.twitch_subs.find({"topic": str(topic)}, {"id": True})
         return await cursor.fetch_next
 
-    async def is_subscription_verified(self, topic: str):
-        cursor = self.sonata.db.twitch_subs.find({"topic": topic}, {"verified": True})
+    async def is_subscription_verified(self, topic: twitch.webhook.Topic):
+        cursor = self.sonata.db.twitch_subs.find(
+            {"topic": str(topic)}, {"verified": True}
+        )
         if await cursor.fetch_next:
             return cursor.next_object()["verified"]
 
@@ -321,36 +324,37 @@ class TwitchMixin(core.Cog):
     @_twitch.command(name="add")
     @limit_subs()
     async def twitch_add(self, ctx: core.Context, user: TwitchUserConverter()):
-        topic = f"{STREAMS_URL}?user_id={user.id}"
+        topic = StreamChanged(user.id)
         callback = f"{CALLBACK_URL}/streams/{user.id}"
+        subscription = self.twitch.create_subscription(
+            callback, topic, 864000, ctx.bot.config["twitch"].hub_secret,
+        )
         alert_config = SubscriptionAlertConfig(id=ctx.guild.id)
         if await self.is_subscription_exist(topic):
-            sub_doc = await ctx.db.twitch_subs.find_one_and_update(
-                {"topic": topic},
-                {"$addToSet": {"guilds": alert_config.dict()}},
-                return_document=ReturnDocument.AFTER,
+            await ctx.db.twitch_subs.update_one(
+                {"topic": str(topic)}, {"$addToSet": {"guilds": alert_config.dict()}},
             )
-            sub = TwitchSubscription(**sub_doc)
         else:
             now = ctx.message.created_at
-            sub = TwitchSubscription(
+            sub_status = TwitchSubscriptionStatus(
                 created_at=now,
                 guilds=[alert_config],
                 id=user.id,
                 login=user.login,
-                topic=topic,
+                topic=str(topic),
                 callback=callback,
-                secret=ctx.bot.config["twitch"].hub_secret,
-            )
-            await ctx.db.twitch_subs.insert_one(sub.dict())
+            ).dict()
+            await ctx.db.twitch_subs.insert_one(sub_status)
         if not await self.is_subscription_verified(topic):
             async with ctx.typing():
-                if not await self.extend_subscription(sub, 864000):
+                try:
+                    await subscription.subscribe()
+                except twitch.HTTPException:
                     return await ctx.inform(_("An error occurred while subscribing."))
 
                 expires_at = datetime.utcnow() + timedelta(seconds=864000)
                 await ctx.db.twitch_subs.update_one(
-                    {"topic": topic}, {"$set": {"expires_at": expires_at}},
+                    {"topic": str(topic)}, {"$set": {"expires_at": expires_at}},
                 )
                 self._have_data.set()
         await ctx.inform(
@@ -366,10 +370,10 @@ class TwitchMixin(core.Cog):
 
     @_twitch.command(name="remove")
     async def twitch_remove(self, ctx: core.Context, user: TwitchUserConverter()):
-        topic = STREAMS_URL + f"?user_id={user.id}"
+        topic = StreamChanged(user.id)
         if await self.is_subscription_exist(topic):
             result = await ctx.db.twitch_subs.update_one(
-                {"topic": topic, "guilds.id": ctx.guild.id},
+                {"topic": str(topic), "guilds.id": ctx.guild.id},
                 {"$pull": {"guilds": {"id": ctx.guild.id}}},
             )
             if result.modified_count != 0:

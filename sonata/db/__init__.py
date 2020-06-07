87cd2c742e965c2fe72c5d5cec8cf5c75347ce78
Fozar <fozar97@mail.ru>
2020-06-07 16:14:14 +0700

    Added discord authorization on the website

--- sonata/db/__init__.py
+++ sonata/db/__init__.py
@@ -1,5 +1,7 @@
 import asyncio
 
+from aiohttp_session import setup
+from aiohttp_session_mongo import MongoStorage
 from motor import motor_asyncio as motorio
 
 from sonata.db.logging import setup_logger
@@ -11,8 +13,10 @@ async def close_mongo(app):
 
 async def init_db(app):
     setup_logger()
-    app["db"] = motorio.AsyncIOMotorClient(
+    app["db"] = db = motorio.AsyncIOMotorClient(
         app["config"]["mongo"].url, appname="sonata", io_loop=asyncio.get_event_loop()
     )[app["config"]["mongo"].database]
     app["logger"].info("Mongo connected.")
     app.on_cleanup.append(close_mongo)
+    session_collection = db.sessions
+    setup(app, MongoStorage(session_collection, max_age=3600 * 24 * 30))
--- sonata/views/__init__.py
+++ sonata/views/__init__.py
@@ -1,5 +1,6 @@
 from .cogs import CogList, Cog
 from .guild import Guild
+from .login import Login, LoginCallback
 
 
 async def init_views(app):
@@ -7,3 +8,5 @@ async def init_views(app):
     cors.add(app.router.add_route("*", "/guilds", Guild))
     cors.add(app.router.add_route("*", "/cogs", CogList))
     cors.add(app.router.add_route("*", r"/cogs/{name}", Cog))
+    cors.add(app.router.add_route("*", "/login", Login))
+    cors.add(app.router.add_route("*", "/login/callback", LoginCallback))

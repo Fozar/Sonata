4531a9beb8c69d27df179db88b2e8c2f6179d9d7
Fozar <fozar97@mail.ru>
2020-06-11 17:42:16 +0700

    Added routes to guild, guild stats and guild emojis

--- sonata/views/__init__.py
+++ sonata/views/__init__.py
@@ -1,11 +1,14 @@
 from .cogs import CogList, Cog
-from .guild import Guild
+from .guilds import Guilds, Guild, GuildStats, GuildEmojis
 from .auth import Auth, AuthCallback
 
 
 async def init_views(app):
     cors = app["cors"]
-    cors.add(app.router.add_route("*", "/guilds", Guild))
+    cors.add(app.router.add_route("*", "/guilds", Guilds))
+    cors.add(app.router.add_route("*", r"/guilds/{id}", Guild))
+    cors.add(app.router.add_route("*", r"/guilds/{id}/stats", GuildStats))
+    cors.add(app.router.add_route("*", r"/guilds/{id}/emojis", GuildEmojis))
     cors.add(app.router.add_route("*", "/cogs", CogList))
     cors.add(app.router.add_route("*", r"/cogs/{name}", Cog))
     cors.add(app.router.add_route("*", "/auth", Auth))

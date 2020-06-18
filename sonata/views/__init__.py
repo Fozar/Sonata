47a49d3624b66a7791ab2d7cfc2a53a477f8f558
Fozar <fozar97@mail.ru>
2020-06-18 17:05:11 +0700

    Added guild members endpoint

--- sonata/views/__init__.py
+++ sonata/views/__init__.py
@@ -1,5 +1,5 @@
 from .cogs import CogList, Cog
-from .guilds import Guilds, Guild, GuildStats, GuildEmojis
+from .guilds import Guilds, Guild, GuildStats, GuildEmojis, GuildMembers
 from .auth import Auth, AuthCallback, AuthLogout
 
 
@@ -9,6 +9,7 @@ async def init_views(app):
     cors.add(app.router.add_route("*", r"/guilds/{id}", Guild))
     cors.add(app.router.add_route("*", r"/guilds/{id}/stats", GuildStats))
     cors.add(app.router.add_route("*", r"/guilds/{id}/emojis", GuildEmojis))
+    cors.add(app.router.add_route("*", r"/guilds/{id}/members", GuildMembers))
     cors.add(app.router.add_route("*", "/cogs", CogList))
     cors.add(app.router.add_route("*", r"/cogs/{name}", Cog))
     cors.add(app.router.add_route("*", "/auth", Auth))
--- sonata/views/guilds.py
+++ sonata/views/guilds.py
@@ -93,3 +93,55 @@ class GuildEmojis(web.View, CorsViewMixin):
             raise web.HTTPNotFound
 
         return web.json_response({"id": guild_id, "emojis": emojis_stats})
+
+
+class GuildMembers(web.View, CorsViewMixin):
+    async def get(self):
+        try:
+            user_id = int(await check_authorized(self.request))
+            guild_id = int(self.request.match_info["id"])
+        except TypeError:
+            raise web.HTTPBadRequest
+
+        bot = self.request.app.get("bot")
+        guild = await check_guild(bot, guild_id)
+        if guild.owner.id != user_id:
+            raise web.HTTPForbidden
+
+        limit = min(int(self.request.query.get("limit", "0")), 100)
+        cursor = bot.db.user_stats.find(
+            {"guild_id": guild_id},
+            {
+                "_id": False,
+                "guild_id": False,
+                "created_at": False,
+                "last_exp_at": False,
+                "auto_lvl_msg": False,
+            },
+            sort=[("exp", -1)],
+            limit=limit,
+        )
+        member_stats = await cursor.to_list(None)
+        for member_s in member_stats:
+            member = await bot.db.users.find_one(
+                {"id": member_s["user_id"]}, {"_id": False, "name": True}
+            )
+            if not member:
+                try:
+                    member = guild.get_member(
+                        member_s["user_id"]
+                    ) or await guild.fetch_member(member_s["user_id"])
+                except discord.HTTPException:
+                    member_stats.remove(member_s)
+                    continue
+                else:
+                    member_name = str(member)
+            else:
+                member_name = member["name"]
+
+            member_s["name"] = member_name
+        return web.json_response(
+            text=json.dumps(
+                {"id": guild_id, "members": member_stats}, ensure_ascii=False
+            )
+        )
